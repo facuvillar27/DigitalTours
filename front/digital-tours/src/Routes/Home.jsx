dd3ef923fcace06943c48eca5894dc208b378ba4
Amilcar <amilcarjh24@gmail.com>
2024-10-26 23:47:53 -0300

    DT-02: [Add] card component styles

--- front/digital-tours/src/Routes/Home.jsx
+++ front/digital-tours/src/Routes/Home.jsx
@@ -1,5 +1,19 @@
+import styles from "../styles/home.module.css";
+import Card from "../Components/Card/Card";
+
 const Home = () => {
-  return <div>Hola todos</div>;
+  return (
+    <div className={styles.main}>
+      <div className={styles.categories_menu}>
+        <div className={styles.categories_container}>
+          <p>categories</p>
+        </div>
+      </div>
+      <div className={styles.main_body}>
+        <Card />
+      </div>
+    </div>
+  );
 };
 
 export default Home;

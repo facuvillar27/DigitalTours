66935546f78e62b6c861dd518f991c94cc7664b2
Amilcar <amilcarjh24@gmail.com>
2024-10-27 00:39:02 -0300

    DT-02: [Add] categories section and cta home img

--- front/digital-tours/src/Routes/Home.jsx
+++ front/digital-tours/src/Routes/Home.jsx
@@ -4,10 +4,20 @@ import Card from "../Components/Card/Card";
 const Home = () => {
   return (
     <div className={styles.main}>
-      <div className={styles.categories_menu}>
-        <div className={styles.categories_container}>
-          <p>categories</p>
+      <div className={styles.cta}>
+        <div className={styles.cta_box}>
+          <h1 className={styles.cta_text}>
+            Recuerdos de viajes que nunca olvidaras
+          </h1>
+          <input type="search" />
         </div>
+      </div>
+      <div className={styles.cat_menu}>
+        <p className={styles.cat_btn}>Categoria</p>
+        <p className={styles.cat_btn}>Categoria</p>
+        <p className={styles.cat_btn}>Categoria</p>
+        <p className={styles.cat_btn}>Categoria</p>
+        <p className={styles.cat_btn}>Categoria</p>
       </div>
       <div className={styles.home_body}>
         <Card />

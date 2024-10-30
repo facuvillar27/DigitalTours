8caacfc52c6b064e2d5a0a3a2ac02ef68adcb42c
Amilcar <amilcarjh24@gmail.com>
2024-10-30 00:11:44 -0300

    DT-02: [Add] ProductDetails styles and functionality

--- front/digital-tours/src/Routes/ProductDetails.jsx
+++ front/digital-tours/src/Routes/ProductDetails.jsx
@@ -1,8 +1,9 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import styles from "../styles/productDetails.module.css";
 import data from "../assets/data.json";
 
 const ProductDetails = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const item = data.tours.find((tour) => tour.id.toString() === id);
 
@@ -14,10 +15,17 @@ const ProductDetails = () => {
 
   return (
     <div className={styles.detail}>
-      <img src={item.image} alt={item.nombre} className={styles.image} />
-      <h2>{item.nombre}</h2>
-      <p>{item.descripcion}</p>
-      <h3>{item.precio}</h3>
+      <div className={styles.detail_box}>
+        <button onClick={() => navigate(-1)} className={styles.backButton}>
+          Volver
+        </button>
+        <div className={styles.detail_info}>
+          <h2>{item.nombre}</h2>
+          <img src={item.image} alt={item.nombre} className={styles.image} />
+          <p>{item.descripcion}</p>
+          <h3>{item.precio}</h3>
+        </div>
+      </div>
     </div>
   );
 };

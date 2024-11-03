2c55de3dcc54a4e4077232ebfee4a7c15734a574
Amilcar <amilcarjh24@gmail.com>
2024-11-03 10:57:20 -0300

    DT-01: [Add] responsive functionaltity to the navbar and footer section

--- front/digital-tours/src/Components/Navbar/Navbar.jsx
+++ front/digital-tours/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,23 @@
 import { Link } from "react-router-dom";
 import styles from "./navbar.module.css";
 import logo from "../../assets/Logo_digitaltours-black.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [color, setColor] = useState(false);
+  const changeColor = () => {
+    window.scrollY > 50 ? setColor(true) : setColor(false);
+  };
+  window.addEventListener("scroll", changeColor);
+
   return (
-    <nav>
+    <nav className={color ? styles.scrolled : styles.navbar}>
       <Link to="/" className={styles.nav_logo}>
         <img src={logo} alt="logo digital tours" className={styles.logo} />
       </Link>
+      <FontAwesomeIcon className={styles.hamburger} icon={faBars} />
       <ul>
         <li>
           <Link to="/login" className={styles.nav_link}>
